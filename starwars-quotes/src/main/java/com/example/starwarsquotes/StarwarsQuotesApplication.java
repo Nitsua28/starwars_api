6eecd680bbd66c6fced96cc62ee32b663fc25387
Nitsua28 <achan113@ucmerced.edu>
2023-07-12 10:45:48 -0500

    skeleton backend

---
+++ starwars-quotes/src/main/java/com/example/starwarsquotes/StarwarsQuotesApplication.java
@@ -0,0 +1,13 @@
+package com.example.starwarsquotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarwarsQuotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarwarsQuotesApplication.class, args);
+	}
+
+}
